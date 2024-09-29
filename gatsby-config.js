9febc19a21228927fa2e76ac7ba58793a2091cf8
Kevin Soto <kevinsoto832@gmail.com>
2024-09-29 15:09:36 -0500

    commiting 9/29/24

--- gatsby-config.js
+++ gatsby-config.js
@@ -39,5 +39,17 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: `gatsby-source-wordpress`,
+      options: {
+        url: `http://netlify.local/graphql`,
+        schema: {
+          timeout: 30000,
+        },
+        debug: {
+          preview: true,
+        },
+      },
+    },
   ],
 }
--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -1,5 +1,6 @@
 import * as React from "react"
 import { Link } from "gatsby"
+import { graphql } from "gatsby"
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
