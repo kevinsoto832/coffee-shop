c59073d591f9e965d8ca28445ba52bfe48f26085
Kevin Soto <43228622+kevinsoto832@users.noreply.github.com>
2024-10-20 15:16:35 -0500

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -31,8 +31,7 @@ module.exports = {
             // username: "statement",
             // password: "psychedelic",
             username: process.env.GATSBY_HTTPBASICAUTH_USERNAME || "statement",
-            password:
-              process.env.GATSBY_HTTPBASICAUTH_PASSWORD || "psychedelic",
+            password: process.env.GATSBY_HTTPBASICAUTH_PASSWORD || "psychedelic",
             // username: process.env.HTTPBASICAUTH_USERNAME,
             // password: process.env.HTTPBASICAUTH_PASSWORD,
           },
